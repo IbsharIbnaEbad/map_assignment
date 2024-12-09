de17a5b5747e01a159be6b308205731d83d897d6
Ibshar <cse_182210012101018@lus.ac.bd>
2024-12-09 23:55:41 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/map_assignment_ostad/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.map_assignment_ostad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:map_assignment_ostad/home_screen.dart';
+
+void main() {
+  runApp(const GoogleMapsApp());
+}
+
+class GoogleMapsApp extends StatelessWidget {
+  const GoogleMapsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
